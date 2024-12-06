91cb88a17bf77b605e5bd6c1e6c4bea5ef07cac2
David Bustamante <66386035+D4v13B@users.noreply.github.com>
2024-12-06 08:57:21 -0500

    fix routes page

--- frontend/src/pages/RoutesPage.jsx
+++ frontend/src/pages/RoutesPage.jsx
@@ -39,33 +39,48 @@ export const RoutesPage = () => {
               </tr>
             </thead>
             <tbody>
-              {routes.map((e) => (
-                <tr key={e.id} className="hover:bg-indigo-50">
-                  <td className="py-3 px-6 text-sm font-medium text-gray-900 border-b">
-                    {e.id}
-                  </td>
-                  <td className="py-3 px-6 text-sm font-medium text-gray-900 border-b">
-                    {e.drivers.name}
-                  </td>
-                  <td className="py-3 px-6 text-sm font-medium text-gray-900 border-b">
-                    {new Date(e.date).toLocaleDateString("es-PA")}
-                  </td>
-                  <td className="py-3 px-6 text-sm font-medium text-blue-600 hover:text-blue-800 border-b">
-                    <Link 
-                    to={`/router-detail/${e.id}`}
-                    onClick={event => handleEditRoute(event, e, `/router-detail/${e.id}`)}>
-                      Ver
-                    </Link>
+              {routes.length > 0 ? (
+                routes.map((e) => (
+                  <tr key={e.id} className="hover:bg-indigo-50">
+                    <td className="py-3 px-6 text-sm font-medium text-gray-900 border-b">
+                      {e.id}
+                    </td>
+                    <td className="py-3 px-6 text-sm font-medium text-gray-900 border-b">
+                      {e.drivers.name}
+                    </td>
+                    <td className="py-3 px-6 text-sm font-medium text-gray-900 border-b">
+                      {new Date(e.date).toLocaleDateString("es-PA")}
+                    </td>
+                    <td className="py-3 px-6 text-sm font-medium text-blue-600 hover:text-blue-800 border-b">
+                      <Link
+                        to={`/router-detail/${e.id}`}
+                        onClick={(event) =>
+                          handleEditRoute(event, e, `/router-detail/${e.id}`)
+                        }
+                      >
+                        Ver
+                      </Link>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td
+                    colSpan="4"
+                    className="py-3 px-6 text-sm font-medium text-gray-500 border-b text-center"
+                  >
+                    No hay rutas disponibles.
                   </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
         </div>
-        <Link 
-        to={"/add-route"} 
-        className="bg-indigo-600 px-5 py-2 text-white text-sm font-medium uppercase rounded-lg  hover:bg-indigo-700">
-              + Añadir Ruta
+        <Link
+          to={"/add-route"}
+          className="bg-indigo-600 px-5 py-2 text-white text-sm font-medium uppercase rounded-lg  hover:bg-indigo-700"
+        >
+          + Añadir Ruta
         </Link>
       </div>
     </>
